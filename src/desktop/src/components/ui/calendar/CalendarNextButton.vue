f8206111278ff167a6aec6f29c6a4c845cea0b78
CarlosShao <1933942520@qq.com>
2026-07-23 16:59:19 +0800

    style: remove unused calendar-heading styles and buttonVariants imports

--- src/desktop/src/components/ui/calendar/CalendarNextButton.vue
+++ src/desktop/src/components/ui/calendar/CalendarNextButton.vue
@@ -5,7 +5,6 @@ import { ChevronRight } from 'lucide-vue-next'
 import { reactiveOmit } from '@vueuse/core'
 import { CalendarNext, useForwardProps } from 'reka-ui'
 import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
 
 const props = defineProps<CalendarNextProps & { class?: HTMLAttributes['class'] }>()
 
